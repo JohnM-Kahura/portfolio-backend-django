067afb0acfffac50a7308c70748a2a106de58836
JohnM-Kahura <Johnmungai392@gmail.com>
2021-12-14 21:36:40 +0300

    maintance

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings    
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -12,3 +13,5 @@ urlpatterns = [
     path('', include("work.urls"),name='work'),
     path('', include("contact.urls"),name='contact'),
 ]
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
--- work/models.py
+++ work/models.py
@@ -4,7 +4,7 @@ class Work(models.Model):
     icon=models.CharField( max_length=255)
     title=models.CharField( max_length=255)
     desc=models.CharField( max_length=255)
-    img=models.CharField( max_length=255)
+    img=models.ImageField(('Image'),upload_to=upload_to,default='work/default.png')
     def __str__(self):
         return self.title
 
